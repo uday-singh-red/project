d944facf027b148357a6cb1b1c6ac5ba28599169
uday singh <us511818@gmail.com>
2026-04-28 04:34:39 -0700

    user registation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,64 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
 import { asynHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { Response } from "../utils/response.js";
 
 const registerUser= asynHandler( async(req, res)=>{
-    res.status(200).json({
-        message:"welcome user"
-    })
+   
+   const {fullname , email, username, password } =req.body;
+
+   console.log(req.body)
+
+     if ([username,email,fullname,password].some((field)=>field?.trim()===""))
+         {
+         throw new ApiError(400,"all field should be complete")
+        }
+
+        const existingUser=await User.findOne(
+           {
+            $or:[{email}, {username}]
+           }
+        )
+
+        if(existingUser){
+            throw new ApiError(409,"user is already registerd")
+        }
+        console.log(existingUser)
+
+       const avatarLocalPath= req.files?.avatar[0]?.path;
+       const coverImageLocalPath= req.files?.coverImage[0]?.path;
+
+       if (!avatarLocalPath) {
+        throw new ApiError(400," avatar is mendatory ")
+       }
+
+       const avatar= await uploadOnCloudinary(avatarLocalPath)
+       const coverImage= await uploadOnCloudinary(coverImageLocalPath)
+
+       if (!avatar) {
+         throw new ApiError(400," avatar is not uploaded ")
+       }
+
+     const user = await User.create({
+                fullname,
+                username: username.toLowerCase(),
+                email,
+                avatar: avatar.url,
+                coverimage: coverImage?.url || ""
+            })
+
+     const createUser=await User.findById(user._id).select("-password -referstoken")
+
+     if(!createUser){
+        throw new ApiError(500, "something went wrong during registration")
+     }
+    
+    return res.status(201).json(
+        new Response(200,createUser,"user registered successflly")
+     )
+
+
 })
 
 export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -69,7 +69,7 @@ const userSchema = new Schema  (
         });
      
         userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password);
+        return await bcrypt.compare(password, this.password);
      };
 
      userSchema.methods.generateAccessToken= function(){
